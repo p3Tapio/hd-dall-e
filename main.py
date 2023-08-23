0b13b54958ca20b2874b7d011a2262565410e398
Petri Laine <petri.laine@almamedia.fi>
2023-08-23 13:31:30 +0300

    Move secrets to env, add n param to route

--- main.py
+++ main.py
@@ -1,31 +1,34 @@
 import openai
 from fastapi import FastAPI
 from pydantic import BaseModel
+from dotenv import load_dotenv, find_dotenv
+import os
 
+load_dotenv(find_dotenv())
 
 openai.api_type = "azure"
-openai.api_base = ""
+openai.api_base = os.getenv("API_URL")
 openai.api_version = "2023-06-01-preview"
-openai.api_key = ""
+openai.api_key = os.getenv("API_KEY")
 
 app = FastAPI()
 
 class Request(BaseModel):
     prompt: str
+    n: int
 
 @app.post("/img")
 def get_image(prompt: Request):
     prompt_value = prompt.prompt 
+    amount = prompt.n
     
     response = openai.Image.create(
         prompt=prompt_value,
         size='1024x1024',
-        n=1
+        n=amount
     )
 
-
-    image_url = response["data"][0]["url"]
-    return {"imageUrl": image_url}
+    return {"data": response["data"]}
 
 
 
